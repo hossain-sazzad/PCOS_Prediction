1c8037b756e6f4eaf8ae37909743c616a2044827
Sazzad Hossain <hossainsazzad95@yahoo.com>
2023-04-01 13:10:32 +0600

    adds navbar

--- frontend/pcos_frontend/src/App.js
+++ frontend/pcos_frontend/src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '@mui/material/Button';
-import {Backdrop, Box, CircularProgress, Paper} from "@mui/material";
-import Home from "./home";
+import logo from './logo.svg'
+import './App.css'
+import Button from '@mui/material/Button'
+import {
+  AppBar,
+  Backdrop,
+  Box,
+  CircularProgress,
+  Paper,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import Home from './home'
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+      <AppBar position="static">
+        <Toolbar>
+          <Box display="flex" justifyContent="center" width="100%">
+            <div className={'pr-8 hover:text-gray-50 cursor-pointer'}>
+              <Typography variant="h4">Health Care</Typography>
+            </div>
+            <div className={'pr-8 hover:text-gray-50 cursor-pointer'}>
+              <Typography variant="h4">PCOS Prediction</Typography>
+            </div>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
